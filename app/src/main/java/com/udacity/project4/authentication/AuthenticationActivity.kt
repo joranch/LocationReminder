e789cf6b7acb7eb74310593c04ca89af7f6fd2e8
Jøran Haines <joran.haines@gmail.com>
2022-01-21 18:12:07 +0530

    initial commit

---
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -0,0 +1,101 @@
+package com.udacity.project4.authentication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.observe
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
+
+/**
+ * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
+ * signed in users to the RemindersActivity.
+ */
+
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "AuthenticationActivity"
+        const val SIGN_IN_REQUEST_CODE = 1001
+    }
+
+    private val viewModel: AuthenticationViewModel by viewModels()
+    private lateinit var binding: ActivityAuthenticationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+        binding.loginButton.setOnClickListener {
+            //launchSignInFlow()
+            val intent = Intent(this, RemindersActivity::class.java)
+            startActivity(intent)
+        }
+
+        observeAuthenticationState()
+
+
+//          TODO: a bonus is to customize the sign in flow to look nice using :
+        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+
+    }
+
+    private fun launchSignInFlow() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            SIGN_IN_REQUEST_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i(
+                    TAG,
+                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+            } else {
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun observeAuthenticationState() {
+        viewModel.authenticationState.observe(this) {
+            when (it) {
+                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+                    Log.d("TAG", "Authenticated")
+                    val intent = Intent(this, RemindersActivity::class.java)
+                    startActivity(intent)
+                }
+                AuthenticationViewModel.AuthenticationState.UNAUTHENTICATED -> {
+                    Log.d("TAG", "Not authenticated")
+                    Toast.makeText(this, "Couldn't authenticate user", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationViewModel.kt
@@ -0,0 +1,18 @@
+package com.udacity.project4.authentication
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+
+class AuthenticationViewModel  : ViewModel() {
+    enum class AuthenticationState {
+        AUTHENTICATED, UNAUTHENTICATED
+    }
+
+    val authenticationState = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationState.AUTHENTICATED
+        } else {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
+}
